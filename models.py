f57344e59a3ddf918c222fd2afc8d9352c94f10c
G. J. Yeomans <n8hth@protonmail.com>
2025-06-25 22:07:38 -0400

    Add subscriptions model due to foreign key in users model (#1)

--- models.py
+++ models.py
@@ -3,8 +3,23 @@
 # external imports
 from flask_sqlalchemy import SQLAlchemy
 
+# Initialize the database
 db=SQLAlchemy()
 
+# Subscriptions model
+class subscriptions(db.Model):
+  __tablename__ = 'subscriptions'
+  subscription_id = db.Column(db.Integer, primary_key=True)
+  subscription_name = db.Column(db.Text)
+  rentals_allowed = db.Column(db.Integer)
+  price = db.Column(db.Text)
+
+  def __init__(self, subscription_name, rentals_allowed, price):
+    self.subscription_name = subscription_name
+    self.rentals_allowed = rentals_allowed
+    self.price = price
+
+# Users model
 class users(db.Model):
   __tablename__ = 'users'
   user_id = db.Column(db.Integer, primary_key=True)
@@ -19,7 +34,7 @@ class users(db.Model):
   subscription_id = db.Column(db.Integer, db.ForeignKey('subscriptions.subscription_id'))
   access_level = db.Column(db.Integer)
 
-  def __init__(self, first_name, last_name, email, address, city, state, zip, password, subscription_id, access_level):
+  def __init__(self, first_name, last_name, email, address, city, state, zip_code, password, subscription_id, access_level):
     self.first_name = first_name
     self.last_name = last_name
     self.email = email
