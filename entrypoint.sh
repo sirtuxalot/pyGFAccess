d8e6953dfd3bdbffbfc6a01215ebca079b1ce673
G. J. Yeomans <n8hth@protonmail.com>
2025-07-15 07:33:20 -0400

    Add commands to create and deploy migrations automatically

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #/usr/bin/env sh
 
+flask --app access.py db migrate 2>&1 | tee /var/log/pyGFAccess
+flask --app access.py db upgrade 2>&1 | tee -a /var/log/pyGFAccess
+
 /usr/local/bin/uwsgi --ini /pyGFAccess/uwsgi.ini
