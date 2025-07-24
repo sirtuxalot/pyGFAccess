4baea7e6ed337e62ecdd21522949b13af98c836d
G. J. Yeomans <n8hth@protonmail.com>
2025-07-23 22:35:55 -0400

    Update register tests, but would like to work out deleting of registration test user automatically

--- tests/test_02_register.py
+++ tests/test_02_register.py
@@ -1,5 +1,27 @@
+# tests/test_02_register.py
+
+# local imports
 from access import app
+# external imports
+import base64
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
 import json
+
+# application settings
+
+# support functions
+
+"""Read public key to encrypt the outgoing string"""
+def load_public_key():
+  with open('keys/public_key.pem', 'rb') as pem_file:
+    public_key = serialization.load_pem_public_key(pem_file.read())
+  return public_key
+
+"""Encrypt the ougoing password"""
+def encrypt_password(message, public_key):
+  encrypted = public_key.encrypt(message.encode('utf-8'), padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+  return base64.b64encode(encrypted).decode('utf-8')
 
 # Register
 
@@ -13,7 +35,7 @@ def test_register_success():
     "city": "Lawton",
     "state": "Oklahoma",
     "zip_code": "73452",
-    "password": "$2b$12$ZYuzjqJzR3dj2gzOYkPCwuzKtJLP1oQCPJJTgVCyPfbl2sfYgkBoO",
+    "password": encrypt_password('SadiePass', load_public_key()),
     "subscription_id": 4,
     "access_level": 99
   }
@@ -24,20 +46,34 @@ def test_register_success():
   )
   assert response.status_code == 201
   assert b'{"access_level":99,"address":"next county over","city":"Lawton","email":"sy0623@mail.io","first_name":"Sadie","last_name":"Yeomans","message":"SUCCESS: User successfully registered!","state":"Oklahoma","subscription_id":4,"zip_code":"73452"}\n' in response.data
+  """Ensure the provided email acquires the appropriate first and last name"""
+  data =  {
+    "email": "sy0623@mail.io",
+    "password": encrypt_password('SadiePass', load_public_key())
+  }
+  response = app.test_client().post(
+    '/login',
+    data=json.dumps(data),
+    headers={"Content-Type": "application/json"}
+  )
+  assert response.status_code == 200
+  assert json.loads(response.data)["email"] == data["email"]
+  assert json.loads(response.data)["first_name"] == "Sadie"
+  assert json.loads(response.data)["last_name"] == "Yeomans"
 
 def test_register_failure_dup_email():
   """This will registration attempt will fail since there is an existing email in the users table"""
   data =  {
-    "first_name": "Ruby",
+    "first_name": "Sadie",
     "last_name": "Yeomans",
-    "email": "ry0517@proton.me",
-    "address": "up the street",
-    "city": "Delton",
-    "state": "Michigan",
-    "zip_code": "49046",
-    "password": "$2b$12$8vdXMfFuTCBS0vcW9HSwn.0FTGGmHDre.Y0vAcwcB0WC29rpNgrFC",
-    "subscription_id": 3,
-    "access_level": 99,
+    "email": "sy0623@mail.io",
+    "address": "next county over",
+    "city": "Lawton",
+    "state": "Oklahoma",
+    "zip_code": "73452",
+    "password": encrypt_password('SadiePass', load_public_key()),
+    "subscription_id": 4,
+    "access_level": 99
   }
   response = app.test_client().post(
     '/register',
